6c4be8b6b6ff1065282e011dc28362ae23e823a6
Harry Lekishon <harry.lekishon@student.moringaschool.com>
2022-06-07 18:01:22 +0300

    functions and parameters

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+distanceFromHqInBlocks = (block) => {
+    return (block >= 42) ? block -42 : 42 -block;
+}
+function distanceFromHqInFeet(feet){
+    return distanceFromHqInBlocks(feet)*264;
+}
+
+function distanceTravelledInFeet (start, destination){
+    return start > destination ? (start-destination)*264 :(destination-start)*264;
+}
+function calculatesFarePrice(start, destination){
+
+    let distance = distanceTravelledInFeet(start,destination);
+
+    if(distance< 400){
+        return 0;
+    }
+    if(400< distance && distance < 2000){
+        return (distance-400) *0.02;
+    }
+    if(distance > 2000 && distance <= 2500){
+        return 25;
+    }
+    else if (distance >2500){
+        return "cannot travel that far";
+    }
+}
